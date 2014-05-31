65e4a089d3ffe259e7462f2c44048d540307ae8a
idle sign <idlesign@yandex.ru>
2014-05-31 20:22:26 +0700

    Module __init__ changes.

--- webapi/__init__.py
+++ webapi/__init__.py
@@ -1,27 +1,31 @@
-from deluge.plugins.init import PluginInitBase
-
-
 VERSION = (0, 1, 0)
 
 
-class CorePlugin(PluginInitBase):
+try:
+    from deluge.plugins.init import PluginInitBase
 
-    def __init__(self, plugin_name):
-        from core import Core as _plugin_cls
-        self._plugin_cls = _plugin_cls
-        super(CorePlugin, self).__init__(plugin_name)
+    class CorePlugin(PluginInitBase):
 
-
-class GtkUIPlugin(PluginInitBase):
-
-    def __init__(self, plugin_name):
-        from gtkui import GtkUI as _plugin_cls
-        self._plugin_cls = _plugin_cls
-        super(GtkUIPlugin, self).__init__(plugin_name)
+        def __init__(self, plugin_name):
+            from core import Core as _plugin_cls
+            self._plugin_cls = _plugin_cls
+            super(CorePlugin, self).__init__(plugin_name)
 
 
-class WebUIPlugin(PluginInitBase):
-    def __init__(self, plugin_name):
-        from webui import WebUI as _plugin_cls
-        self._plugin_cls = _plugin_cls
-        super(WebUIPlugin, self).__init__(plugin_name)
+    class GtkUIPlugin(PluginInitBase):
+
+        def __init__(self, plugin_name):
+            from gtkui import GtkUI as _plugin_cls
+            self._plugin_cls = _plugin_cls
+            super(GtkUIPlugin, self).__init__(plugin_name)
+
+
+    class WebUIPlugin(PluginInitBase):
+        def __init__(self, plugin_name):
+            from webui import WebUI as _plugin_cls
+            self._plugin_cls = _plugin_cls
+            super(WebUIPlugin, self).__init__(plugin_name)
+
+except ImportError:
+    # That's what we do when this import prevents docs from being built.
+    pass
