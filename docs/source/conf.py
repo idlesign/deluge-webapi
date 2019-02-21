643833b572bfb4c89cc8603029a10a5ed0c435f3
idle sign <idlesign@yandex.ru>
2019-02-21 20:42:18 +0700

    Years updated.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'deluge-webapi'
-copyright = u'2014-2018, Igor `idle sign` Starikov'
+copyright = u'2014-2019, Igor `idle sign` Starikov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
